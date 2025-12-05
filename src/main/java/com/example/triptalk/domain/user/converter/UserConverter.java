276829e4716c5bda5df86c3ac3ad0d553e9abe7c
Yujin1219 <jin-1219@naver.com>
2025-12-06 01:29:09 +0900

    [#3] Refactor: Converter 메서드 static으로 변경

--- src/main/java/com/example/triptalk/domain/user/converter/UserConverter.java
+++ src/main/java/com/example/triptalk/domain/user/converter/UserConverter.java
@@ -2,12 +2,10 @@ package com.example.triptalk.domain.user.converter;
 
 import com.example.triptalk.domain.user.dto.UserResponse;
 import com.example.triptalk.domain.user.entity.User;
-import org.springframework.stereotype.Component;
 
-@Component
 public class UserConverter {
 
-    public UserResponse.UserInfoDTO toUserInfoDTO(User user) {
+    public static UserResponse.UserInfoDTO toUserInfoDTO(User user) {
         return UserResponse.UserInfoDTO.builder()
                 .userId(user.getId())
                 .email(user.getEmail())
